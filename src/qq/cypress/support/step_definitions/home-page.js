b3dae8ef2380c42da3ddb3bdc3030080e910f3dd
Henry Shen <shenhanrui@outlook.com>
2021-09-27 22:07:03 +0800

    Rename project

--- src/qq/cypress/support/step_definitions/home-page.js
+++
@@ -1,9 +0,0 @@
-import { When } from "cypress-cucumber-preprocessor/steps";
-
-When(`I open home page`, () => {
-    cy.visit('/')
-})
-
-When(`I open qq page`, () => {
-    cy.visit('https://qq.com')
-})
